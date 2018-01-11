b590eb7a4a1be9ef0c19172b6eb96aa01b89d934
lilixiang3701@163.com <suspense>
2018-01-11 17:55:38 +0800

    读取图片数据

--- main.py
+++ main.py
@@ -6,10 +6,32 @@
 # @File    : main.py
 # @Software: PyCharm Community Edition
 
+# 7 将图像转化为二维向量，并转换红蓝像素
 
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import re
+import matplotlib.pyplot as plt # plt 用于显示图片
+import matplotlib.image as mpimg # mpimg 用于读取图片
+import numpy as np
+import os
+
+
+# 无法使用相对路径，因此使用绝对的文件路径
+lena = mpimg.imread(os.path.dirname(os.path.realpath(__file__))+'/sampledatas/america.png') # 读取和代码处于同一目录下的 lena.png
+# 此时 lena 就已经是一个 np.array 了，可以对它进行任意处理
+lena.shape #(512, 512, 3)
+
+plt.imshow(lena) # 显示图片
+# plt.axis('off') # 不显示坐标轴
+# plt.show()
+
+lena_1 = lena[:,:,2]
+print(lena_1[0:12])
+plt.imshow(lena_1)
+plt.show()
+
+
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+# import re
 
 # html = urlopen("http://blog.csdn.net/hy245120020/article/details/50776197")
 # bsObj = BeautifulSoup(html,"html.parser")
@@ -87,30 +109,32 @@ import re
 
 # 6 Python数据可视化——使用Matplotlib创建散点图
 
-from numpy import *
-import numpy as np
-from  sampledatas.gradedata import creatematrixdata
-import matplotlib
-from matplotlib import pyplot
+# from numpy import *
+# import numpy as np
+# from  sampledatas.gradedata import creatematrixdata
+# import matplotlib
+# from matplotlib import pyplot
+#
+# group,labels = creatematrixdata("sampledatas/dataForKNN.txt",3)
+# print(group[0:10],labels[0:10])
+# zhfont = matplotlib.font_manager.FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-microhei.ttc')
+# fig = pyplot.figure()
+#     # pyplot.xlabel(u'Test横轴X',fontproperties=zhfont)
+#     # pyplot.ylabel(u'Test纵轴Y',fontproperties=zhfont)
+# ax = pyplot.subplot(111)
+#     # ax.scatter(group[:,1],group[:,2],15.0*array(labels),15.0*array(labels))
+# labels = np.array(labels)
+#     # print(labels[0:6])
+# idx_1 = np.where(labels==1)
+# p1 = ax.scatter(group[idx_1,0],group[idx_1,1],marker="_",color="r",label='1',s=20)
+# idx_2 = np.where(labels==2)
+# p2 = ax.scatter(group[idx_2,0],group[idx_2,1],marker="+",color="y",label='2',s=20)
+# idx_3 = np.where(labels==3)
+# p3 = ax.scatter(group[idx_3,0],group[idx_3,1],marker="*",color="g",label='3',s=20)
+# pyplot.xlabel(u'每年的飞行里程数',fontproperties=zhfont)
+# pyplot.ylabel(u'玩视频游戏所消耗的事件百分比',fontproperties=zhfont)
+# ax.legend((p1,p2,p3),(u'不喜欢',u'一般',u'超爱'),loc = 2,prop=zhfont)
+#
+# pyplot.show()
 
-group,labels = creatematrixdata("sampledatas/dataForKNN.txt",3)
-print(group[0:10],labels[0:10])
-zhfont = matplotlib.font_manager.FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-microhei.ttc')
-fig = pyplot.figure()
-# pyplot.xlabel(u'Test横轴X',fontproperties=zhfont)
-# pyplot.ylabel(u'Test纵轴Y',fontproperties=zhfont)
-ax = pyplot.subplot(111)
-# ax.scatter(group[:,1],group[:,2],15.0*array(labels),15.0*array(labels))
-labels = np.array(labels)
-# print(labels[0:6])
-idx_1 = np.where(labels==1)
-p1 = ax.scatter(group[idx_1,0],group[idx_1,1],marker="_",color="r",label='1',s=20)
-idx_2 = np.where(labels==2)
-p2 = ax.scatter(group[idx_2,0],group[idx_2,1],marker="+",color="y",label='2',s=20)
-idx_3 = np.where(labels==3)
-p3 = ax.scatter(group[idx_3,0],group[idx_3,1],marker="*",color="g",label='3',s=20)
-pyplot.xlabel(u'每年的飞行里程数',fontproperties=zhfont)
-pyplot.ylabel(u'玩视频游戏所消耗的事件百分比',fontproperties=zhfont)
-ax.legend((p1,p2,p3),(u'不喜欢',u'一般',u'超爱'),loc = 2,prop=zhfont)
 
-pyplot.show()
